6cc07472c02282b001d84f98d2e9f08cc20a067b
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2020-10-28 18:25:31 +0100

    Generate latest tag by default on push tag event (#5)
    Co-authored-by: CrazyMax <crazy-max@users.noreply.github.com>

--- __tests__/meta.test.ts
+++ __tests__/meta.test.ts
@@ -377,10 +377,11 @@ describe('push tag', () => {
       } as Inputs,
       {
         version: 'release1',
-        latest: false
+        latest: true
       } as Version,
       [
-        'user/app:release1'
+        'user/app:release1',
+        'user/app:latest'
       ],
       [
         "org.opencontainers.image.title=Hello-World",
@@ -400,10 +401,11 @@ describe('push tag', () => {
       } as Inputs,
       {
         version: '20200110-RC2',
-        latest: false
+        latest: true
       } as Version,
       [
-        'user/app:20200110-RC2'
+        'user/app:20200110-RC2',
+        'user/app:latest'
       ],
       [
         "org.opencontainers.image.title=Hello-World",
@@ -686,11 +688,13 @@ describe('latest', () => {
       } as Inputs,
       {
         version: 'v1.1.1',
-        latest: false
+        latest: true
       } as Version,
       [
         'org/app:v1.1.1',
+        'org/app:latest',
         'ghcr.io/user/app:v1.1.1',
+        'ghcr.io/user/app:latest',
       ],
       [
         "org.opencontainers.image.title=Hello-World",
@@ -951,10 +955,11 @@ describe('release', () => {
       } as Inputs,
       {
         version: 'v1.1.1',
-        latest: false
+        latest: true
       } as Version,
       [
         'user/app:v1.1.1',
+        'user/app:latest',
       ],
       [
         "org.opencontainers.image.title=Hello-World",
