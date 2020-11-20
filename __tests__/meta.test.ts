6f270f37d4a72665eb4ce53d08ac0cd020684957
CrazyMax <crazy-max@users.noreply.github.com>
2020-11-20 16:30:57 +0100

    Add test

--- __tests__/meta.test.ts
+++ __tests__/meta.test.ts
@@ -928,6 +928,32 @@ describe('latest', () => {
         "org.opencontainers.image.licenses=MIT"
       ]
     ],
+    [
+      'event_tag_v1.1.1.env',
+      {
+        images: ['org/app', 'ghcr.io/MyUSER/MyApp'],
+        tagMatchLatest: false,
+      } as Inputs,
+      {
+        main: 'v1.1.1',
+        partial: [],
+        latest: false
+      } as Version,
+      [
+        'org/app:v1.1.1',
+        'ghcr.io/myuser/myapp:v1.1.1',
+      ],
+      [
+        "org.opencontainers.image.title=Hello-World",
+        "org.opencontainers.image.description=This your first repo!",
+        "org.opencontainers.image.url=https://github.com/octocat/Hello-World",
+        "org.opencontainers.image.source=https://github.com/octocat/Hello-World",
+        "org.opencontainers.image.version=v1.1.1",
+        "org.opencontainers.image.created=2020-01-10T00:30:00.000Z",
+        "org.opencontainers.image.revision=90dd6032fac8bda1b6c4436a2e65de27961ed071",
+        "org.opencontainers.image.licenses=MIT"
+      ]
+    ],
   ])('given %p event ', tagsLabelsTest);
 });
 
